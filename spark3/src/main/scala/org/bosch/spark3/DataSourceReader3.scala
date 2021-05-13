eadfe364a7260316562cf0f95d4a36e846144e18
Rad Rares <rad.rares1@gmail.com>
2021-05-13 17:38:57 +0300

    read multiple files

--- spark3/src/main/scala/org/bosch/spark3/DataSourceReader3.scala
+++ spark3/src/main/scala/org/bosch/spark3/DataSourceReader3.scala
@@ -1,11 +1,33 @@
 package org.bosch.spark3
 
-import org.apache.spark.sql.{Column, SparkSession, functions}
+import org.apache.spark.sql.{Column, Dataset, SparkSession, functions}
 import org.bosch.common.domain.Record
+
+import java.io.File
 
 object DataSourceReader3 {
 
+  def getListOfFiles(dir: String):List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def loadDataSet(session:SparkSession,path:String):Dataset[Record] = {
+    import session.implicits._
+
+    session.read
+      .format("org.bosch.spark3.CustomBinary")
+      .load(path)
+      .as[Record]
+  }
+
   def main(args: Array[String]): Unit = {
+
+    val paths = getListOfFiles("common/src/main/scala/org/bosch/common/out/out")
 
     val ss = SparkSession
       .builder()
@@ -13,18 +35,13 @@ object DataSourceReader3 {
       .master("local[*]")
       .getOrCreate()
 
-    import ss.implicits._
+    val total = paths.map(e => loadDataSet(ss,e.getPath.replace('\\','/'))).reduce(_ union _)
 
-    val dataset = ss.read
-      .format("org.bosch.spark3.CustomBinary")
-      .load("common/src/main/scala/org/bosch/common/out/a.txt")
-      .as[Record]
+    //total.show()
 
-    dataset.show()
-
-//    val b = dataset.filter("parameter.name = 'ch_1'")
-//    b.show
-//    b.explain(true)
+    val a = total.filter(total("filename") === "b.txt")
+    a.show()
+    a.explain(true)
 
   }
 }
